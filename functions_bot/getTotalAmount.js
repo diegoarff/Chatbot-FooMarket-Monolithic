75386f385198b337ac2aac908ed344d244d98d76
Andresc06 <andrescontreras370@gmail.com>
2022-07-06 18:15:19 -0400

    feat: add Card Payment Method

---
+++ functions_bot/getTotalAmount.js
@@ -0,0 +1,18 @@
+let { API_DB, ENDPOINTS_CARTS } = require('~ins');
+
+async function getTotalAmount(userId) {
+
+    try {
+
+        let cartRes = await API_DB.get(ENDPOINTS_CARTS.GET_CART+`?userId=${ userId }`);
+        let cart = cartRes.data;
+        let t_amount = cart[0].total_amount;
+        return t_amount;
+    }
+
+    catch (err) {
+        console.log(err);
+    }
+};
+
+module.exports = getTotalAmount;
