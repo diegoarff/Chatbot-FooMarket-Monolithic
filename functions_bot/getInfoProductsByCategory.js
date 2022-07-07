187459d9e707a2b1f482806213dedc0f2c6fb147
diegoarff <97266392+diegoarff@users.noreply.github.com>
2022-07-06 22:44:45 -0400

    Update getInfoProductsByCategory.js

--- functions_bot/getInfoProductsByCategory.js
+++ functions_bot/getInfoProductsByCategory.js
@@ -7,13 +7,14 @@ async function getInfoProductsByCategory(category) {
         let res = await API_DB.get(ENDPOINTS_PRODUCTS.GET_PRODUCTS_CATEGORY + `?category=${ category }` );
         let items = res.data;
     
-        let space;
         let result = ``;
         let len = items.length;
         let i = 0;
+        for(; i < len; i++) {
             //Toma los resultados del fetch y los va iterando
-            result += `<b>${items[i].id})</b> ${items[i].name.substring(0, 16)} - $${items[i].price}\n${space}`;
-    
+            result += `<b>${items[i].id})</b> ${items[i].name.substring(0, 16)} - $${items[i].price}\n`;
+        }
+        
         return result;
 
     } catch (err) {
